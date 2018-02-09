1e9e6f129a350eaa59d9db960bacbf17dd6724aa
Speymanhs <peyman.xt@gmail.com>
2018-02-09 08:51:35 +0330

    forming structures
    Iformed the structures and a simplemodel for the main which can be written as a function itself

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
-
+struct st_passed
+{
+	char course_code[8];
+	char course_name[19];
+	int course_numofunits;
+	char course_teacher[21];
+};
+struct student
+{
+	string st_firstname;
+	string st_lastname;
+	char st_idnumber[6];
+	struct st_courses;
+};
 int main()
 {
-	cout << "finally";
-    return 0;
+	int a;
+	cin >> a;
+	switch (a)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+	case 12:
+
+	}
 }
