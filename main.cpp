00c78e9761a139ce2d8a9b97d7e161a6d2593574
Speymanhs <peyman.xt@gmail.com>
2018-02-09 12:21:08 +0330

    wrote the initialized structures on a file

--- main.cpp
+++ main.cpp
@@ -18,7 +18,24 @@ struct lesson {
     int lessonUnit;
     char lessonTeacher[21];
 };
+void meghdardehiavvalie()
+{
+	Student initial_student = {0,"","",""};
+	FILE *st=fopen("student.txt","w");
+	for (long long int i = 0; i < 1000000; i++)
+	{
+		fwrite(&initial_student, sizeof(Student), 1, st);
+	}
+	fclose(st);
+	Lesson initial_lesson = { "","",0,"" };
+	FILE *les = fopen("course.txt", "w");
+	for (long long int i = 0; i < 1000000; i++)
+	{
+		fwrite(&initial_lesson, sizeof(Lesson), 1, les);
+	}
+	fclose(les);
 
+}
 int main()
 {
     return 0;
