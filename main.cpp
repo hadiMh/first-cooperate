fd98fa12146045e7f34fb93eac2d75ecac561f1a
Speymanhs <peyman.xt@gmail.com>
2018-02-09 17:54:38 +0330

    made the creating a new lesson function more convenient to read

--- main.cpp
+++ main.cpp
@@ -29,42 +29,77 @@ void createFilesForFirstExecution()
 	fclose(file_lessons);
 
 }
-/*
-	This function adds a lesson to lesson.txt
-*/
-void Create_New_Lesson()
+/* Creating 'students' file and 'lessons' file at the very first execution.
+This function should be executed once at the very first execution of the software so be careful.
+After firs execution of the software comment this function's name in the 'main' scope*/
+void createFilesForFirstExecution()
 {
-	Lesson New_sample_structure, Existing_samples;
-	FILE* File_New_Lesson = fopen("lessons.txt", "r+");
+	FILE* file_students = fopen("students.txt", "w");
+	fclose(file_students);
+	FILE* file_lessons = fopen("lessons.txt", "w");
+	fclose(file_lessons);
 
-	cout << "Please enter the lesson code (Remember the password must contain less than8 characters!) :" << endl;
-	cin.get(New_sample_structure.leassonCode, 8);
-	cin.ignore(10000, '\n');
-	while (!feof(File_New_Lesson))
+}
+/*
+This function check wheter the lesson we are trying to add already exists or not
+returns 1 if it doesn't exist
+returns -1 if it already exists
+*/
+int doesThisLessonAlreadyExists(Lesson newSampleStructure, FILE*fileNewLesson)
+{
+	Lesson existingSamples;
+	while (!feof(fileNewLesson))
 	{
-		fread(&Existing_samples, sizeof(Lesson), 1, File_New_Lesson);
-		if (strcmp(Existing_samples.leassonCode, New_sample_structure.leassonCode) == 0)
+		fscanf(fileNewLesson, "%s %s %d %s\n", &existingSamples.leassonCode, &existingSamples.lessonName, &existingSamples.lessonUnit, &existingSamples.lessonTeacher);
+
+		if (strcmp(existingSamples.leassonCode, newSampleStructure.leassonCode) == 0)
 		{
 			cout << "THis lesson code already exists!" << endl;
-			fseek(File_New_Lesson, 0, SEEK_SET);
-			cout << "Please enter the lesson code again(Remember the password must contain less than8 characters!) :" << endl;
-			cin.get(New_sample_structure.leassonCode, 8);
-			cin.ignore(10000, '\n');
+			return -1;
 		}
 	}
+	return 1;
+}
+/*
+This function scans the information needed for creatng a new lesson
+*/
+int getInformationForCreatingNewLesson()
+{
+	FILE* fileNewLesson = fopen("lessons.txt", "r+");
+	Lesson newSampleStructure, existingSamples;
+	cout << "Please enter the lesson code (Remember the code must contain less than8 characters!) :" << endl;
+	cin.get(newSampleStructure.leassonCode, 8);
+	cin.ignore(10000, '\n');
+	if (doesThisLessonAlreadyExists(newSampleStructure, fileNewLesson) == -1)
+	{
+		fclose(fileNewLesson);
+		return -1;
+	}
 	cout << "Now you should enter the lesson name (Name contains at most 18 characters!) :" << endl;
-	cin.get(New_sample_structure.lessonName, 19);
+	cin.get(newSampleStructure.lessonName, 19);
 	cin.ignore(10000, '\n');
 	cout << "Now you should enter the teacher's name:(At most 20 characters !) :" << endl;
-	cin.getline(New_sample_structure.lessonTeacher, 21);
+	cin.getline(newSampleStructure.lessonTeacher, 21);
 	cout << "And finally you should enter this lesson's units : " << endl;
-	cin >> New_sample_structure.lessonUnit;
+	cin >> newSampleStructure.lessonUnit;
 	cin.ignore(10000, '\n');
-	fseek(File_New_Lesson, 0, SEEK_END);
-	fwrite(&New_sample_structure, sizeof(Lesson), 1, File_New_Lesson);
-	fclose(File_New_Lesson);
+	fseek(fileNewLesson, 0, SEEK_END);
+	fprintf(fileNewLesson, "%s %s %d %s\n", newSampleStructure.leassonCode, newSampleStructure.lessonName, newSampleStructure.lessonUnit, newSampleStructure.lessonTeacher);
+	fclose(fileNewLesson);
+	return 1;
 }
-
+/*
+This function is used to create a new lesson
+*/
+void createNewLesson()
+{
+	if (getInformationForCreatingNewLesson() == 1)
+	{
+		cout << "This Record has been successfully added!" << endl;
+	}
+	else
+		cout << "Adding a Lesson failed due to entering existing Lesson code!" << endl;
+}
 void firstPanel()
 {
     int inputNumber;
